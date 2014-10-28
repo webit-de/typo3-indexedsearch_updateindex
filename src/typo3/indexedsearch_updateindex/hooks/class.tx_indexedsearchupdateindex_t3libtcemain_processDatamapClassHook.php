cea70b6e8b950a3fb377d4394034b4ccb1773fbb
Dan Untenzu <untenzu@webit.de>
2014-10-28 16:13:39 +0100

    [TASK] CGL

--- src/typo3/indexedsearch_updateindex/hooks/class.tx_indexedsearchupdateindex_t3libtcemain_processDatamapClassHook.php
+++ src/typo3/indexedsearch_updateindex/hooks/class.tx_indexedsearchupdateindex_t3libtcemain_processDatamapClassHook.php
@@ -18,30 +18,30 @@ class tx_indexedsearchupdateindex_t3libtcemain_processDatamapClassHook {
 		// (change page properties or page content)
 		if(($status == 'new' || $status == 'update') && $table == 'pages') {
 			// Get pHashes of current record
-			$pHashesOfGivenPage_result = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows('phash','index_section', 'page_id='.intval($id));
+			$pHashesOfCurrentPage = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows('phash', 'index_section', 'page_id=' . intval($id));
 
 			// Count fetched old pHashes
-			$cntPHashes = count($pHashesOfGivenPage_result);
+			$countPHashes = count($pHashesOfCurrentPage);
 
 			// Create array with old pHashes for deleting
-			$deleteFetchedPHashes = array();
-			for($i=0; $i<$cntPHashes; $i++) {
-				$deleteFetchedPHashes[] = $pHashesOfGivenPage_result[$i]['phash'];
+			$deletePHashes = array();
+			for($i=0; $i<$countPHashes; $i++) {
+				$deletePHashes[] = $pHashesOfCurrentPage[$i]['phash'];
 			}
 
 			// Delete old pHashes from index tables
-			$where_clausel = 'phash IN ('.implode(',',$GLOBALS['TYPO3_DB']->cleanIntArray($deleteFetchedPHashes)).')';
-			$tables_to_clear = explode(',', 'index_section,index_rel,index_phash,index_grlist,index_fulltext,index_fulltext,index_debug');
-			foreach ($tables_to_clear as $table) {
-				$GLOBALS['TYPO3_DB']->exec_DELETEquery($table, $where_clausel);
+			$where = 'phash IN (' . implode(',', $GLOBALS['TYPO3_DB']->cleanIntArray($deletePHashes)) . ')';
+			$tables = explode(',', 'index_section,index_rel,index_phash,index_grlist,index_fulltext,index_fulltext,index_debug');
+			foreach ($tables as $table) {
+				$GLOBALS['TYPO3_DB']->exec_DELETEquery($table, $where);
 			}
 
 			// Run curl process in background to visit the updated page
-			$url_to_open = t3lib_div::getIndpEnv('TYPO3_SITE_URL').'index.php?id='.$id; // url for updated record
+			$url = t3lib_div::getIndpEnv('TYPO3_SITE_URL') . 'index.php?id=' . $id; // url for updated record
 			$curl = curl_init();
-			curl_setopt($curl, CURLOPT_URL, $url_to_open);
+			curl_setopt($curl, CURLOPT_URL, $url);
 			curl_setopt($curl, CURLOPT_HEADER, false); // don't include HEADER to output
-			curl_setopt($curl, CURLOPT_NOBODY, true); // don't include BODY(content) to output
+			curl_setopt($curl, CURLOPT_NOBODY, true); // don't include BODY (content) to output
 			curl_exec($curl);
 			curl_close($curl);
 		}
